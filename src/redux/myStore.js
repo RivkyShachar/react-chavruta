b673c4f5ebba1459519254cc54000cdae798b014
RivkyShachar <rvider@g.jct.ac.il>
2023-12-31 20:59:50 +0200

    toggleSlice delete

--- src/redux/myStore.js
+++ src/redux/myStore.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import requestSlice from './featchers/requestSlice'
 import userSlice from './featchers/userSlice'
-import toggleSlice from './featchers/toggleSlice'
 import authSlice from './featchers/authSlice'
 import searchSlice from './featchers/searchSlice'
 
@@ -10,7 +9,6 @@ const myStore = configureStore({
   reducer: {
     requestSlice,
     userSlice,
-    toggleSlice,
     searchSlice,
     authSlice,
   }
