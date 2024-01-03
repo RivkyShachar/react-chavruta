2c5a951748901ede4d1eb6b637156f316f66ef28
Yael <yaelgloiberman@gmail.com>
2024-01-03 09:37:29 +0200

    want to study with me

--- src/components/user/userHome.js
+++ src/components/user/userHome.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Header from '../header/headerHome'
 import FilterBarHome from './filterBarHome'
 import RequestList from './requestList'
 
--- src/components/user/userProfileBar.js
+++ src/components/user/userProfileBar.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const UserProfileBar = () => {
+
+    const user = useSelector((myStore)=>myStore.userSlice.user);
+
     return (
         <div className='container bg-info'>
 
             <div className='container col-7 '>
                 <div className='row'>
                     <div className='col-4 mx-2 '>
-                        <h1>Yael Globerman</h1>
+                        <h1>{user.firstName} {user.lastName}</h1>
                     </div>
                     <div className='col-6 py-2 px-5 text-end'>
                         <img
