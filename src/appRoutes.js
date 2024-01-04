de163f6035e3825d3d0a89dc76831be3c759cdca
Yael <yaelgloiberman@gmail.com>
2024-01-04 15:44:05 +0200

    pusg

--- src/appRoutes.js
+++ src/appRoutes.js
@@ -1,4 +1,4 @@
-import React, { Suspense,useEffect } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import authSlice from './redux/featchers/authSlice';
@@ -20,18 +20,18 @@ const RequestList = React.lazy(() => import('./components/user/requestList'));
 const Layout = React.lazy(() => import('./layout/layout'));
 const HeaderAdmin = React.lazy(() => import('./components/header/adminHeader'));
 const UserProfileMy = React.lazy(() => import('./components/user/userProfile'));
-const Error = React.lazy(()=> import("./components/common/error"));
-const UserProfileAdmin = React.lazy(()=>import("./pages/admin/userProfile"));
-const UserProfile = React.lazy(()=>import("./pages/user/userProfile"));
+const Error = React.lazy(() => import("./components/common/error"));
+const UserProfileAdmin = React.lazy(() => import("./pages/admin/userProfile"));
+const UserProfile = React.lazy(() => import("./pages/user/userProfile"));
 
 const AppRoutes = () => {
     const dispatch = useDispatch();
-    const {isLoggedIn, userRole} = useSelector(store => store.authSlice);
+    const { isLoggedIn, userRole } = useSelector(store => store.authSlice);
     useEffect(() => {
         if (localStorage.getItem(TOKEN_NAME)) {
-          handleUserInfo(dispatch);
+            handleUserInfo(dispatch);
         }
-      }, [dispatch]);
+    }, [dispatch]);
 
 
 
@@ -50,43 +50,51 @@ const AppRoutes = () => {
                         {isLoggedIn && userRole === 'user' ?
                             (<>
                                 <Route index element={<UserHome />} />
-                                <Route path='singleUser/:parameter' element={<UserProfile />} />
-                                <Route path='createPost' element={<CreateStudyRequest />} />
-                                <Route path='usersList' element={<UsersList />} />
-                                <Route path='requestsList/:parameter' element={<RequestList />} />                                
-                                <Route path='userProfile/:parameter' element={<UserProfileMy />} />                                
-                                <Route path='marked' element={<RequestList />} />
-                                <Route path='editProfile' element={<SignUp />} />
-                            </>)
-                            :
-                            <></>
+                                <Route
+                                    path="singleUser/:parameter"
+                                    element={
+                                        <Suspense fallback={<div>Loading...</div>}>
+                                            <UserProfile />
+                                        </Suspense>
+                                    }
+                                />
+                        {/* <Route path='singleUser/:parameter' element={<UserProfile />} /> */}
+                        <Route path='createPost' element={<CreateStudyRequest />} />
+                        <Route path='usersList' element={<UsersList />} />
+                        <Route path='requestsList/:parameter' element={<RequestList />} />
+                        <Route path='userProfile/:parameter' element={<UserProfileMy />} />
+                        <Route path='marked' element={<RequestList />} />
+                        <Route path='editProfile' element={<SignUp />} />
+                    </>)
+                    :
+                    <></>
 
                         }
-                    </Route>
-                    <Route path='/admin/*' element={<HeaderAdmin />}>
-                        {isLoggedIn && userRole === 'admin' ?
-                            (<>
-                                <Route index element={<AdminHome />} />
-                                <Route path='singleUserAdmin/:idSingleRequest' element={<UserProfileAdmin />} />
-                                <Route path='singleUser/:idSingle1' element={<SingleUser />} />
-                                <Route path='usersListAdmin' element={<UsersListAdmin />} />
-                                <Route path='profileListImage' element={<ProfileListImage />} />
-                                <Route path='requestList' element={<RequestListAdmin />} />
-                                <Route path='createPost' element={<CreateStudyRequest />} />
-                                <Route path='usersList' element={<UsersList />} />
-                                <Route path='requestsList/:parameter' element={<RequestList />} />                                
-                                <Route path='userProfile/:parameter' element={<UserProfile />} />                                
-                                <Route path='marked' element={<RequestList />} />
+                </Route>
+                <Route path='/admin/*' element={<HeaderAdmin />}>
+                    {isLoggedIn && userRole === 'admin' ?
+                        (<>
+                            <Route index element={<AdminHome />} />
+                            <Route path='singleUserAdmin/:idSingleRequest' element={<UserProfileAdmin />} />
+                            <Route path='singleUser/:idSingle1' element={<SingleUser />} />
+                            <Route path='usersListAdmin' element={<UsersListAdmin />} />
+                            <Route path='profileListImage' element={<ProfileListImage />} />
+                            <Route path='requestList' element={<RequestListAdmin />} />
+                            <Route path='createPost' element={<CreateStudyRequest />} />
+                            <Route path='usersList' element={<UsersList />} />
+                            <Route path='requestsList/:parameter' element={<RequestList />} />
+                            <Route path='userProfile/:parameter' element={<UserProfile />} />
+                            <Route path='marked' element={<RequestList />} />
 
-                            </>)
-                            :
-                            <></>
-                        }
-                    </Route>
-                    <Route path='*' element={<Error />} />
-                </Routes>
-            </Router>
-        </Suspense>
+                        </>)
+                        :
+                        <></>
+                    }
+                </Route>
+                <Route path='*' element={<Error />} />
+            </Routes>
+        </Router>
+        </Suspense >
     );
 };
 
