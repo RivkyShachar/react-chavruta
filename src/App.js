534662f338b11114d505790f54e6ca2a13279c66
Yael <yaelgloiberman@gmail.com>
2024-01-08 13:53:29 +0200

    v

--- src/App.js
+++ src/App.js
@@ -10,19 +10,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 library.add(fas);
 
 function App() {
-
-  // const zoomMeetingLink = "https://us05web.zoom.us/j/88204599411?pwd=XS2IfztdSXPYYVNi288qLM8hKVQTqt.1";
-
-  // const startMeeting = () => {
-  //   window.open(zoomMeetingLink);
-  // }
-
   return (
     <Provider store={myStore}>
 
       <AppRoutes />
-
-      {/* <button onClick={startMeeting}>Start Meeting</button> */}
     </Provider>
   );
 }
--- src/components/auth/register/profileInput.js
+++ src/components/auth/register/profileInput.js
@@ -376,4 +376,3 @@ export default ProfileInput
 
 
 
-
