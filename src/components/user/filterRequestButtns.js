7f7b77c1809f995194f9d94ee89e0f3e52d1f8eb
RivkyShachar <rvider@g.jct.ac.il>
2024-01-11 14:14:02 +0200

    translate state

--- src/components/user/filterRequestButtns.js
+++ src/components/user/filterRequestButtns.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import OtherUserRequestList1 from './otherUserRequestList1';
+import translate from '../../utill/translator';
+import { useSelector } from 'react-redux';
 
 const FilterRequestButtns = () => {
   const [filterState, setFilterState] = useState("close");
+  const language = useSelector((myStore) => myStore.languageSlice.language);
 
   const handleButtonClick = (filterValue) => {
     setFilterState(filterValue);
@@ -10,10 +13,10 @@ const FilterRequestButtns = () => {
 
   return (
     <div className='container'>
-      <button className='btn btn-outline-primary mx-2' onClick={() => handleButtonClick("open")}>Open</button>
-      <button className='btn btn-outline-success mx-2' onClick={() => handleButtonClick("close")}>Close</button>
-      <button className='btn btn-outline-secondary mx-2' onClick={() => handleButtonClick("done")}>Done</button>
-      <button className='btn btn-outline-dark mx-2' onClick={() => handleButtonClick("past")}>Past</button>
+      <button className='btn btn-outline-primary mx-2' onClick={() => handleButtonClick("open")}>{translate('post.open', language)}</button>
+      <button className='btn btn-outline-success mx-2' onClick={() => handleButtonClick("close")}>{translate('post.close', language)}</button>
+      <button className='btn btn-outline-secondary mx-2' onClick={() => handleButtonClick("done")}>{translate('post.done', language)}</button>
+      <button className='btn btn-outline-dark mx-2' onClick={() => handleButtonClick("past")}>{translate('post.past', language)}</button>
 
       <OtherUserRequestList1 state={filterState} />
     </div>
