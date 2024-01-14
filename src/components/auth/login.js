dcc0b88e2a279ee49fda33a17ddaee30c96899d9
RivkyShachar <rvider@g.jct.ac.il>
2024-01-14 14:12:47 +0200

    eye icon and red errors

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -7,33 +7,27 @@ import { verifyToken } from '../../services/apiService';
 import { handleUserInfo } from '../../utill/authService';
 import "../../css/main.css";
 import translate from '../../utill/translator';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const Login = () => {
   const dispatch = useDispatch();
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const nav = useNavigate();
-  const { formState: { errors } } = useForm();
+  const { handleSubmit, register, formState: { errors } } = useForm();
   const language = useSelector((myStore) => myStore.languageSlice.language);
 
-  const onSub = (e) => {
-    e.preventDefault();
+  const onSub = async (data) => {
     setIsSubmitted(true);
-    const data = {
-      email: e.target.email.value,
-      password: e.target.password.value
-    };
-    doApi(data);
-  }
-
-  const doApi = async (_data) => {
     try {
       const url = API_URL + '/auth/login';
-      const data = await doApiRequest(url, "POST", _data);
+      const responseData = await doApiRequest(url, "POST", data);
 
       // Decode the token to access its properties
-      if (data.data.token) {
-        localStorage.setItem(TOKEN_NAME, data.data.token);
-        const decodedToken = data.data.token;
+      if (responseData.data.token) {
+        localStorage.setItem(TOKEN_NAME, responseData.data.token);
+        const decodedToken = responseData.data.token;
         verifyToken(decodedToken);
       }
 
@@ -47,11 +41,14 @@ const Login = () => {
       } else {
         nav("/");
       }
-    }
-    catch (err) {
-      alert("Incorrect email or password")
+    } catch (err) {
+      alert("Incorrect email or password");
       setIsSubmitted(false);
     }
+  }
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
   }
 
   return (
@@ -59,39 +56,44 @@ const Login = () => {
       <div className="wrapper wrapper--w680">
         <div className="card card-4">
           <div className="card-body">
-
             <div className="row row-space">
-
-              <form onSubmit={(e) => onSub(e)}>
-                <div className="col-12 my-1 text-center ">
-                 <h2 className="title label  font-weight-bold">{translate('login.login', language)}</h2>
+              <form onSubmit={handleSubmit(onSub)}>
+                <div className="col-12 my-1 text-center">
+                  <h2 className="title label font-weight-bold">{translate('login.login', language)}</h2>
                 </div>
                 <div className="col-12 my-1 px-4">
                   <label className="label" htmlFor="email">
                     {translate('contact.email', language)}
                   </label>
                   <input
-                    name="email"
-                    className="input--style-4"
+                    {...register("email", { required: true })}
+                    className={`input--style-4 ${errors.email && "input-error"}`}
                     type="email"
                     id="email"
                   />
+                  {errors.email && <p className="error-message text-danger">*Email is required</p>}
                 </div>
                 <div className="col-12 my-1 px-4">
                   <label className="label" htmlFor="password">
                     {translate('user.password', language)}
                   </label>
-                  <input
-                    name="password"
-                    className="input--style-4"
-                    type="password"
-                    id="password"
-                  />
+                  <div className="password-input-container">
+                    <input
+                      {...register("password", { required: true })}
+                      className={`input--style-4 col-11 ${errors.password && "input-error"}`}
+                      type={showPassword ? "text" : "password"}
+                      id="password"
+                    />
+                    <span className="password-toggle-icon" onClick={togglePasswordVisibility}>
+                      <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+                    </span>
+                  </div>
+                  {errors.password && <p className="error-message text-danger">*Password is required</p>}
                 </div>
                 <div className="col-12 d-flex justify-content-center mt-5">
                   <button
                     type="submit"
-                    className="btn btn-register "
+                    className="btn btn-register"
                   >
                     {translate('login.login', language)}
                   </button>
