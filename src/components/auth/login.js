47c62d320c36e09c63f950eebc08d47a63de54ce
MeamnimLab <meamnimidf@gmail.com>
2024-12-01 12:33:47 +0200

    add login code to users

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
@@ -13,13 +13,24 @@ import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 const Login = () => {
   const dispatch = useDispatch();
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false); // Added loading state
   const [showPassword, setShowPassword] = useState(false);
   const nav = useNavigate();
   const { handleSubmit, register, formState: { errors } } = useForm();
   const language = useSelector((myStore) => myStore.languageSlice.language);
 
+  useEffect(()=>{
+    alert(
+        "🎉 Welcome! You can log in and explore the app using these credentials:\n\n" +
+        "✨ Username: gilad@gmail.com\n" +
+        "✨ Password: Gilad1234\n\n" +
+        "🌟 Note: Since we're using a free server, it might take a few minutes to wake up if it's been idle. Grab a coffee ☕, and we'll be ready in no time!"
+      );          
+  },[])
+
   const onSub = async (data) => {
     setIsSubmitted(true);
+    setLoading(true); // Start loading
     try {
       const url = API_URL + '/auth/login';
       const responseData = await doApiRequest(url, "POST", data);
@@ -44,6 +55,8 @@ const Login = () => {
     } catch (err) {
       alert("Incorrect email or password");
       setIsSubmitted(false);
+    } finally {
+      setLoading(false); // Stop loading
     }
   }
 
@@ -94,10 +107,16 @@ const Login = () => {
                   <button
                     type="submit"
                     className="btn btn-register"
+                    disabled={loading}
                   >
-                    {translate('login.login', language)}
+                    {loading ? "Loading..." : translate('login.login', language)} {/* Show loading */}
                   </button>
                 </div>
+                {loading && (
+                  <div className="col-12 text-center mt-3">
+                    <p>Loading, please wait...</p>
+                  </div>
+                )}
               </form>
             </div>
           </div>
@@ -108,3 +127,4 @@ const Login = () => {
 }
 
 export default Login;
+
